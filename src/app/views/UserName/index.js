29b633d11c5d7cea0d19eeb18cfee1116dace214
john lemuel zarraga <jlzd1994@gmail.com>
2021-07-18 19:24:56 -0400

    Avance en renderizado de info de usuario al visualizarlo

--- src/app/views/UserName/index.js
+++ src/app/views/UserName/index.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import userImage from "../../../assets/images/default-user-image.png";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import Swal from "sweetalert2";
+import history from "../../../components/History";
+import axios from "axios";
+import helpers from "../../../components/Helpers";
 
 import {
     Switch,
@@ -9,12 +12,63 @@ import {
     Redirect
   } from "react-router-dom";
 
+  const { swalOffBackend } = helpers;
 
 /*Componente para manejar nombre del usuario*/
 export const UserName = ({location}) => {
 
+    const [ profileName, setProfileName ] = useState("");
+    const [ socialMedia, setSocialMedia ] = useState([]);
+
     const { pathname } = location;
     const username = pathname.replace("/", "");
+
+    const payload = {
+        username: username,
+    };
+
+    useEffect(() => {
+        axios.post("/users/usernameData", payload).then((res) => {
+
+            const { ok, msg, data } = res.data;
+
+            if(ok && msg === "Username Profile Data found."){
+              
+                const { profileFullName, profileBio, socialMedia } = data;
+                setProfileName(profileFullName);
+
+                setSocialMedia(socialMedia);
+            }
+    
+        })
+        .catch((e) => {
+            if (e.response === undefined) {
+                swalOffBackend();
+                history.push("/");
+                return 1;
+            }
+
+            const { msg, ok } = e.response.data;
+            if (!ok) {
+              Swal.fire({
+                title: "Error",
+                text: msg,
+                icon: "error",
+                confirmButtonText: "Try again",
+              })
+              .then((result) => {
+                if (result.isConfirmed) {
+                  history.push("/login");
+                } else {
+                  history.push("/login");
+                }
+              });
+
+
+            }
+        });
+
+      }, [])
 
     const viewPrivateLinks = () => {
         Swal.fire({
@@ -37,84 +91,93 @@ export const UserName = ({location}) => {
             </div>
 
             <div className="row d-flex justify-content-center h5">
-                <h3 style={{ color: "white" }}>John Doe ({username}).</h3>
+                <h3 style={{ color: "white" }}>{profileName} ({username}).</h3>
             </div>
 
             <div className="row">
                 <div className="col-sm-12 text-center">
-                    <a  href="/edit-profile" className="mr-2 btn btn-primary btn-sm center-block" Style="width: 100px;">Edit Profile</a>
-                    <a className="ml-2 btn btn-primary btn-sm center-block" Style="width: 100px;">Sign Out</a>
+                    <a  href="/edit-profile" className="mr-2 btn btn-primary btn-sm center-block" style={{width: "100px"}}>Edit Profile</a>
+                    <a className="ml-2 btn btn-primary btn-sm center-block" style={{width: "100px"}}>Sign Out</a>
                 </div>
             </div>
 
             <div className="row d-flex justify-content-center h5">
-                <div className="m-2 card col-sm-3">
-                    <div className="card-body">
-                            <span className="btn-sm" style={{backgroundColor: '#FF0080', 
-                                borderRadius: '15px',
-                                textAlign: 'center', color: 'white' }}
-                            >
-                                <i className="bi bi-instagram"> </i></span>&#160; 
-                            <label style={{ color: "black" }}>Instagram</label>
+                {socialMedia.map((elemento, index) => (
+                    <div key={index} className="m-2 card col-sm-3">
+                        <div className="card-body">
+                            { 
+                                /*Según la red que responde el servicio, condicionamos que red social mostrar*/
+                                (elemento.socialNetwork === "Youtube") ?
+                                <>
+                                    <span className="btn-sm" style={{backgroundColor: 'red', 
+                                        borderRadius: '15px',
+                                        textAlign: 'center', color: 'white' }}
+                                    >
+                                    <i className="bi bi-youtube"> </i></span>&#160; 
+                                    <label style={{ color: "black" }}> {elemento.profile}</label>
+                                </> : 
+                                ((elemento.socialNetwork === "Twitter") ? 
+                                <>
+                                    <span className="btn-sm" style={{backgroundColor: '#1DA1F2', 
+                                    borderRadius: '15px',
+                                    textAlign: 'center', color: 'white' }}
+                                    >
+                                    <i className="bi bi-twitter"></i></span>&#160; 
+                                    <label style={{ color: "black" }}> {elemento.profile}</label>
+                                </> : 
+                                (elemento.socialNetwork === "Instagram") ?
+                                <>
+                                    <span className="btn-sm" style={{backgroundColor: '#FF0080', 
+                                        borderRadius: '15px',
+                                        textAlign: 'center', color: 'white' }}
+                                    >
+                                        <i className="bi bi-instagram"></i></span>&#160; 
+                                    <label style={{ color: "black" }}> {elemento.profile}</label>
+                                </> : 
+                                 (elemento.socialNetwork === "Facebook") ? 
+                                 <>
+                                    <span className="btn-sm" style={{backgroundColor: '#3b5998', 
+                                        borderRadius: '15px',
+                                        textAlign: 'center', color: 'white' }}
+                                    >
+                                    <i className="bi bi-facebook"></i></span>&#160; 
+                                    <label style={{ color: "black" }}> {elemento.profile}</label>
+                                 </> : (elemento.socialNetwork === "Email") ?
+                                 <>
+                                    <span className="btn-sm" style={{backgroundColor: 'gray', 
+                                        borderRadius: '15px',
+                                        textAlign: 'center', color: 'white' }}
+                                    >
+                                        <i className="bi bi-envelope-fill"></i></span>&#160; 
+                                    <label style={{ color: "black" }}> Email</label>
+                                 </> : (elemento.socialNetwork === "Phone Number") ? 
+                                 <>
+                                    <span className="btn-sm" style={{backgroundColor: 'black', 
+                                        borderRadius: '15px',
+                                        textAlign: 'center', color: 'white' }}
+                                    >
+                                        <i className="bi bi-person-lines-fill"></i></span>&#160; 
+                                    <label style={{ color: "black" }}>Phone Number</label>
+                                 </> : (elemento.socialNetwork === "Snapchat") ? 
+                                 <></> : (elemento.socialNetwork === "Soundcloud") ? 
+                                 <></> : (elemento.socialNetwork === "Linkedin") ? 
+                                 <>
+                                    <span className="btn-sm" style={{backgroundColor: '#0e76a8', 
+                                            borderRadius: '15px',
+                                            textAlign: 'center', color: 'white' }}
+                                        >
+                                        <i className="bi bi-linkedin"></i></span>&#160; 
+                                        <label style={{ color: "black" }}> {elemento.profile}</label>
+                                 </> : <></>
+                                )
+                            }
+                        </div>
                     </div>
-                </div>
-
-                <div className="m-2 card col-sm-3">
-                    <div className="card-body">
-                            <span className="btn-sm" style={{backgroundColor: 'black', 
-                                borderRadius: '15px',
-                                textAlign: 'center', color: 'white' }}
-                            >
-                            |<i className="bi bi-person-lines-fill"></i></span>&#160; 
-                            <label style={{ color: "black" }}>Phone Number</label>
-                    </div>
-                </div>
-
-                <div className="m-2 card col-sm-3">
-                    <div className="card-body">
-                            <span className="btn-sm" style={{backgroundColor: 'red', 
-                                borderRadius: '15px',
-                                textAlign: 'center', color: 'white' }}
-                            >
-                                <i className="bi bi-youtube"> </i></span>&#160; 
-                            <label style={{ color: "black" }}> Youtube</label>
-                    </div>
-                </div>
+                ))}
             </div>
 
-            <div className="row d-flex justify-content-center h5">
-                <div className="m-2 card col-sm-3">
-                    <div className="card-body">
-                            <span className="btn-sm" style={{backgroundColor: '#0e76a8', 
-                                borderRadius: '15px',
-                                textAlign: 'center', color: 'white' }}
-                            >
-                            <i className="bi bi-linkedin"></i></span>&#160; 
-                            <label style={{ color: "black" }}> Liknedin</label>
-                    </div>
-                </div>
-                <div className="m-2 card col-sm-3">
-                    <div className="card-body">
-                            <span className="btn-sm" style={{backgroundColor: '#1DA1F2', 
-                                borderRadius: '15px',
-                                textAlign: 'center', color: 'white' }}
-                            >
-                                <i className="bi bi-twitter"></i></span>&#160; 
-                            <label style={{ color: "black" }}> Twitter</label>
-                    </div>
-                </div>
-                <div className="m-2 card col-sm-3">
-                    <div className="card-body">
-                            <span className="btn-sm" style={{backgroundColor: 'gray', 
-                                borderRadius: '15px',
-                                textAlign: 'center', color: 'white' }}
-                            >
-                                <i className="bi bi-envelope-fill"></i></span>&#160; 
-                            <label style={{ color: "black" }}> Email</label>
-                    </div>
-                </div>
-            </div>
-
+            
+            {/* Comantado ya que se usará para la info privada
             <div className="row d-flex justify-content-center h5">
                 <div className="m-2 card col-sm-3" onClick={viewPrivateLinks} style={{cursor: 'pointer'}}>
                     <div className="card-body">
@@ -126,7 +189,7 @@ export const UserName = ({location}) => {
                         &#160;<label style={{ color: "black" }}> Private</label>
                     </div>
                 </div>
-            </div>
+            </div>*/ }
         </>
     )
 }
