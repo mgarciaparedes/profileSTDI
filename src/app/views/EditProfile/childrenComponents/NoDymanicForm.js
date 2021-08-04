a0c2c1cdc39513e3d2524770b83e968a28050ced
Miguel Garcia <miguelgarciaparedes22@gmail.com>
2021-08-04 13:00:45 -0400

    refactor: modularización de componente EditProfile

---
+++ src/app/views/EditProfile/childrenComponents/NoDymanicForm.js
@@ -0,0 +1,257 @@
+import React from "react";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import * as Icon from "react-bootstrap-icons";
+import Swal from "sweetalert2";
+import { Form, InputGroup, Alert, Button } from "react-bootstrap";
+
+function NoDymanicForm({
+  nameState,
+  bioState,
+  disabledButton,
+  reader,
+  reader2,
+  handleNameChange,
+  handleBioChange,
+  handleOnAdd,
+  onSubmit,
+  clearData,
+  setBase64ImgBanner
+}) {
+  return (
+    <Formik
+      onSubmit={(values, { resetForm }) => {
+        onSubmit(values);
+        resetForm({ values: null });
+      }}
+      initialValues={{
+        fullName: nameState,
+        bio: bioState,
+        socialMedia: null,
+      }}
+    >
+      {({
+        handleSubmit,
+        handleChange,
+        handleBlur,
+        setFieldValue,
+        values,
+        touched,
+        isValid,
+        errors,
+      }) => (
+        <Form
+          onSubmit={handleSubmit}
+          noValidate
+          autoComplete="off"
+          name="addProfileData"
+          id="addProfileeData"
+        >
+          <div className="row">
+            <div className="col-12 col-sm-12">
+              {/*Inicio Campo Profile Fullname*/}
+              <Form.Label className="text-white form-label" htmlFor="basic-url">
+                Profile Full Name:
+              </Form.Label>
+              <InputGroup className="mb-2">
+                <Form.Control
+                  name="fullName"
+                  //values={values.fullName}
+                  defaultValue={nameState}
+                  type="text"
+                  placeholder="Type your profile name"
+                  onChange={handleNameChange}
+                />
+              </InputGroup>
+              {/*Fin Campo Profile Fullname*/}
+              {/*Inicio Campo Profile Photo*/}
+              <InputGroup className="mb-2">
+                <Form.Group controlId="formFile" className="mb-2">
+                  <Form.Label className="text-white form-label">
+                    Profile Photo:
+                  </Form.Label>
+                  <Form.Control
+                    type="file"
+                    onChange={(e) => {
+                      if (e.target.files.length > 0) {
+                        if (
+                          e.target.files[0].type === "image/jpeg" ||
+                          e.target.files[0].type === "image/jpg" ||
+                          e.target.files[0].type === "image/png" ||
+                          e.target.files[0].type === "image/gif"
+                        ) {
+                          reader.readAsDataURL(e.target.files[0]);
+                        } else {
+                          Swal.fire({
+                            title: "Unsupported format",
+                            text: "Format supported: JPG, JPEG, PNG and GIF.",
+                            icon: "info",
+                            confirmButtonText: "OK",
+                          });
+                        }
+                      } else {
+                        setBase64ImgBanner("");
+                      }
+                    }}
+                  />
+                </Form.Group>
+              </InputGroup>
+              {/*Fin Campo Profile Photo*/}
+              {/*Inicio Campo Banner Photo*/}
+              <InputGroup className="mb-2">
+                <Form.Group controlId="formFile2" className="mb-2">
+                  <Form.Label className="text-white form-label">
+                    Banner Photo:
+                  </Form.Label>
+                  <Form.Control
+                    type="file"
+                    onChange={(e) => {
+                      if (e.target.files.length > 0) {
+                        if (
+                          e.target.files[0].type === "image/jpeg" ||
+                          e.target.files[0].type === "image/jpg" ||
+                          e.target.files[0].type === "image/png" ||
+                          e.target.files[0].type === "image/gif"
+                        ) {
+                          reader2.readAsDataURL(e.target.files[0]);
+                        } else {
+                          Swal.fire({
+                            title: "Unsupported format",
+                            text: "Format supported: JPG, JPEG, PNG and GIF.",
+                            icon: "info",
+                            confirmButtonText: "OK",
+                          });
+                        }
+                      } else {
+                        setBase64ImgBanner("");
+                      }
+                    }}
+                  />
+                </Form.Group>
+              </InputGroup>
+              {/*Fin Campo Banner Photo*/}
+              {/*Inicio Campo Profile Bio*/}
+              <Form.Label className="text-white form-label" htmlFor="basic-url">
+                Profile Bio:
+              </Form.Label>
+              <InputGroup className="mb-2">
+                <Form.Control
+                  name="bio"
+                  //values={values.bio}
+                  defaultValue={bioState}
+                  as="textarea"
+                  placeholder="Type your profile name"
+                  onChange={handleBioChange}
+                  style={{ height: "100px" }}
+                />
+              </InputGroup>
+              {/*Fin Campo Profile Bio*/}
+              {/*Inicio Select Social Media Channels*/}
+              <Form.Label className="text-white form-label" htmlFor="basic-url">
+                Social Media Channels:
+              </Form.Label>
+              <Alert variant="info">
+                <Icon.ListCheck size={25} />
+                &nbsp; Click from the drop down to add the social media link.
+              </Alert>
+              <InputGroup>
+                <Form.Control
+                  as="select"
+                  name="socialMedia"
+                  values={values.socialMedia}
+                  onChange={(e) => {
+                    //if ((e.target.value = "CustomURL")) {
+                    //}
+                    handleOnAdd(e);
+                    //handleSocialMedia(e);
+                  }}
+                  className="mb-4"
+                >
+                  <option value="">Choose your media...</option>
+                  <option value="Instagram">Instagram</option>
+                  <option value="Whatsapp">Whatsapp</option>
+                  <option value="Snapchat">Snapchat</option>
+                  <option value="Youtube">Youtube</option>
+                  <option value="Facebook">Facebook</option>
+                  <option value="Soundcloud">Soundcloud</option>
+                  <option value="Linkedin">Linkedin</option>
+                  <option value="Telegram">Telegram</option>
+                  <option value="TikTok">TikTok</option>
+                  <option value="Twitter">Twitter</option>
+                  <option value="Spotify">Spotify</option>
+                  <option value="Apple Music">Apple Music</option>
+                  <option value="Venmo">Venmo</option>
+                  <option value="CashApp">CashApp</option>
+                  <option value="Phone Number">Phone Number</option>
+                  <option value="Paypal">Paypal</option>
+                  <option value="GoFundMe">GoFundMe</option>
+                  <option value="Twitch">Twitch</option>
+                  <option value="Discord">Discord</option>
+                  <option value="HouseParty">HouseParty</option>
+                  <option value="OnlyFans">OnlyFans</option>
+                  <option value="Address">Address</option>
+                  <option value="Email">Email</option>
+                  <option value="SMS">SMS</option>
+                  <option value="Website">Website</option>
+                  <option value="CustomURL">CustomURL</option>
+                  <option value="Embed Youtube Video">Embed Youtube Video</option>
+                </Form.Control>
+              </InputGroup>
+            </div>
+
+            <div className="col-12 col-sm-12 col-md-6">
+              <InputGroup className="mb-2">
+                <Button
+                  type="submit"
+                  variant="primary"
+                  disabled={disabledButton === true}
+                  block
+                >
+                  <div className="d-flex d-inline-block justify-content-center">
+                    <span
+                      className="spinner-border spinner-border-sm mt-1 mr-2"
+                      role="status"
+                      style={{
+                        display:
+                          disabledButton === true ? "inline-block" : "none",
+                      }}
+                      aria-hidden="true"
+                    ></span>
+                    {disabledButton === true ? (
+                      " Loading, please wait..."
+                    ) : (
+                      <>
+                        <Icon.PersonLinesFill className="mt-1" />
+                        &nbsp;&nbsp;
+                        <span>Save Changes</span>
+                      </>
+                    )}
+                  </div>
+                </Button>
+              </InputGroup>
+            </div>
+
+            <div className="col-12 col-sm-12 col-md-6">
+              <InputGroup className="mb-2">
+                <Button
+                  type="button"
+                  variant="danger"
+                  onClick={() => clearData()}
+                  block
+                >
+                  <div className="d-flex d-inline-block justify-content-center">
+                    <Icon.BackspaceFill className="mt-1" />
+                    &nbsp;&nbsp;
+                    <span>Clear Data</span>
+                  </div>
+                </Button>
+              </InputGroup>
+            </div>
+          </div>
+        </Form>
+      )}
+    </Formik>
+  );
+}
+
+export default NoDymanicForm;
---
+++ src/app/views/EditProfile/childrenComponents/Row.js
@@ -0,0 +1,360 @@
+import React from "react";
+import { Form, InputGroup, Button, FormControl } from "react-bootstrap";
+import * as Icon from "react-bootstrap-icons";
+
+import YoutubeIcon from "../../../../assets/svg/youtube.svg";
+import InstagramIcon from "../../../../assets/svg/instagram.svg";
+import TwitterIcon from "../../../../assets/svg/twitter.svg";
+import FacebookIcon from "../../../../assets/svg/facebook.svg";
+import LinkedinIcon from "../../../../assets/svg/linkedin.svg";
+import SnapchatIcon from "../../../../assets/svg/snapchat.svg";
+import AppleMusicIcon from "../../../../assets/svg/apple-music.svg";
+import CashappIcon from "../../../../assets/svg/cashapp.svg";
+import SoundcloudIcon from "../../../../assets/svg/soundcloud.svg";
+import SpotifyIcon from "../../../../assets/svg/spotify.svg";
+import TiktokIcon from "../../../../assets/svg/tiktok.svg";
+import VenmoIcon from "../../../../assets/svg/venmo.svg";
+import PaypalIcon from "../../../../assets/svg/paypal.svg";
+import MapPinIcon from "../../../../assets/svg/locationmap.svg";
+import EmailIcon from "../../../../assets/svg/mail.svg";
+import PhoneIcon from "../../../../assets/svg/phone.svg";
+import WhatsappIcon from "../../../../assets/svg/whatsapp.svg";
+import TelegramIcon from "../../../../assets/svg/telegram.svg";
+import GoFundMeIcon from "../../../../assets/svg/gofundme.svg";
+import TwitchIcon from "../../../../assets/svg/twitch.svg";
+import OnlyFansIcon from "../../../../assets/svg/onlyfans.svg";
+import DiscordIcon from "../../../../assets/svg/discord.svg";
+import HousePartyIcon from "../../../../assets/svg/houseparty.svg";
+import SmsIcon from "../../../../assets/svg/sms.svg";
+import WebsiteIcon from "../../../../assets/svg/website.svg";
+
+//Está función está fuera del render porque si la dejo dentro de la func principal
+//se vuelve a renderizar y no funciona.
+//Es la función que me devuelve el html de las redes sociales => RRSS
+//seleccionadas. Si te fijas yo decido como se está viendo con el valor de view,
+//si view===1 me devuelve la vista de los campos que puedo editar
+//si view===2 me devuelve la vista de las redes sociales en columna derecha
+function Row({
+  onChange,
+  onRemove,
+  view,
+  profile,
+  socialNetwork,
+  linkName,
+  convertStringWithPlus,
+  //socialNetworkIcon,
+  //socialNetworkURL,
+}) {
+  return (
+    <>
+      {view === 1 ? (
+        <div className="row mb-2">
+          <div className="col-12 col-sm-12">
+            <label className="text-white">{socialNetwork}</label>
+            {socialNetwork !== "CustomURL" ? (
+              <InputGroup>
+                <Form.Control
+                  name="profile"
+                  value={profile}
+                  onChange={(e) => onChange("profile", e.target.value)}
+                  placeholder={
+                    socialNetwork === "Instagram"
+                      ? "Instagram username"
+                      : socialNetwork === "Snapchat"
+                      ? "Snapchat username"
+                      : socialNetwork === "Whatsapp"
+                      ? "Country code and phone number"
+                      : socialNetwork === "Youtube"
+                      ? "Type the full link"
+                      : socialNetwork === "Facebook"
+                      ? "Type the full link"
+                      : socialNetwork === "Soundcloud"
+                      ? "Souncloud username"
+                      : socialNetwork === "Linkedin"
+                      ? "Type the full link"
+                      : socialNetwork === "TikTok"
+                      ? "Type the full link"
+                      : socialNetwork === "Twitter"
+                      ? "Twitter username"
+                      : socialNetwork === "Spotify"
+                      ? "Spotify username"
+                      : socialNetwork === "Apple Music"
+                      ? "Apple Music username"
+                      : socialNetwork === "Venmo"
+                      ? "Venmo username"
+                      : socialNetwork === "CashApp"
+                      ? "Cashapp username"
+                      : socialNetwork === "Address"
+                      ? "Type the full address"
+                      : socialNetwork === "Phone Number"
+                      ? "Country code and phone number"
+                      : socialNetwork === "Email"
+                      ? "Type the email address"
+                      : socialNetwork === "SMS"
+                      ? "Country code and phone number"
+                      : socialNetwork === "Paypal"
+                      ? "Paypal username"
+                      : socialNetwork === "OnlyFans"
+                      ? "OnlyFans username"
+                      : socialNetwork === "GoFundMe"
+                      ? "Type the full link"
+                      : socialNetwork === "Telegram"
+                      ? "Telegram username"
+                      : socialNetwork === "Twitch"
+                      ? "Twitch username"
+                      : socialNetwork === "Discord"
+                      ? "Type the full link"
+                      : socialNetwork === "HouseParty"
+                      ? "HouseParty username"
+                      : "Type the full link"
+                  }
+                />
+                <InputGroup.Append>
+                  <Button variant="danger" onClick={onRemove}>
+                    <Icon.XCircleFill />
+                  </Button>
+                </InputGroup.Append>
+              </InputGroup>
+            ) : (
+              <>
+                <InputGroup>
+                  <FormControl
+                    placeholder="Description"
+                    name="linkName"
+                    value={linkName}
+                    onChange={(e) => onChange("linkName", e.target.value)}
+                  />
+                </InputGroup>
+                <InputGroup>
+                  <FormControl
+                    placeholder="Type the full link"
+                    name="profile"
+                    value={profile}
+                    onChange={(e) => onChange("profile", e.target.value)}
+                  />
+                  <InputGroup.Append>
+                    <Button variant="danger" onClick={onRemove}>
+                      <Icon.XCircleFill />
+                    </Button>
+                  </InputGroup.Append>
+                </InputGroup>
+              </>
+            )}
+          </div>
+        </div>
+      ) : view === 2 && socialNetwork !== "CustomURL" ? (
+        <>
+          <div className="border border-link m-2 col-3">
+            <a
+              className="btn-no-style"
+              target="_blank"
+              href={
+                socialNetwork === "Instagram"
+                  ? "https://www.instagram.com/" + profile
+                  : socialNetwork === "Snapchat"
+                  ? "https://www.snapchat.com/add/" + profile
+                  : socialNetwork === "Whatsapp"
+                  ? "https://wa.me/" + profile
+                  : socialNetwork === "Youtube"
+                  ? profile
+                  : socialNetwork === "Facebook"
+                  ? profile
+                  : socialNetwork === "Soundcloud"
+                  ? "https://www.soundcloud.com/add/" + profile
+                  : socialNetwork === "Linkedin"
+                  ? profile
+                  : socialNetwork === "TikTok"
+                  ? profile
+                  : socialNetwork === "Twitter"
+                  ? "https://www.twitter.com/" + profile
+                  : socialNetwork === "Spotify"
+                  ? "https://www.spotify.com/" + profile
+                  : socialNetwork === "Apple Music"
+                  ? "https://music.apple.com/" + profile
+                  : socialNetwork === "Venmo"
+                  ? "https://www.venmo.com/" + profile
+                  : socialNetwork === "CashApp"
+                  ? "https://cash.app/$" + profile
+                  : socialNetwork === "Address"
+                  ? "https://www.google.com/maps/search/" +
+                    convertStringWithPlus(profile)
+                  : socialNetwork === "Phone Number"
+                  ? "tel:" + profile
+                  : socialNetwork === "Email"
+                  ? "mailto:" + profile
+                  : socialNetwork === "SMS"
+                  ? "sms:" + profile
+                  : socialNetwork === "Paypal"
+                  ? "https://paypal.com/" + profile
+                  : socialNetwork === "Telegram"
+                  ? "https://t.me/" + profile
+                  : socialNetwork === "OnlyFans"
+                  ? "https://onlyfans.com/" + profile
+                  : socialNetwork === "GoFundMe"
+                  ? profile
+                  : socialNetwork === "Twitch"
+                  ? "https://twitch.tv/" + profile
+                  : socialNetwork === "Discord"
+                  ? profile
+                  : socialNetwork === "HouseParty"
+                  ? "https://houseparty.com/add/" + profile
+                  : profile
+              }
+            >
+              <div className="pt-3 pb-3">
+                <div className="d-flex justify-content-center">
+                  {socialNetwork === "Instagram" ? (
+                    <img
+                      width="50"
+                      height="50"
+                      src={InstagramIcon}
+                      alt="Instagram"
+                    />
+                  ) : socialNetwork === "Whatsapp" ? (
+                    <img
+                      width="50"
+                      height="50"
+                      src={WhatsappIcon}
+                      alt="Whatsapp"
+                    />
+                  ) : socialNetwork === "Snapchat" ? (
+                    <img
+                      width="50"
+                      height="50"
+                      src={SnapchatIcon}
+                      alt="Snapchat"
+                    />
+                  ) : socialNetwork === "Youtube" ? (
+                    <img
+                      width="50"
+                      height="50"
+                      src={YoutubeIcon}
+                      alt="Youtube"
+                    />
+                  ) : socialNetwork === "Facebook" ? (
+                    <img
+                      width="50"
+                      height="50"
+                      src={FacebookIcon}
+                      alt="Facebook"
+                    />
+                  ) : socialNetwork === "Soundcloud" ? (
+                    <img
+                      width="50"
+                      height="50"
+                      src={SoundcloudIcon}
+                      alt="Soundcloud"
+                    />
+                  ) : socialNetwork === "Linkedin" ? (
+                    <img
+                      width="50"
+                      height="50"
+                      src={LinkedinIcon}
+                      alt="Linkedin"
+                    />
+                  ) : socialNetwork === "TikTok" ? (
+                    <img width="50" height="50" src={TiktokIcon} alt="TikTok" />
+                  ) : socialNetwork === "Twitter" ? (
+                    <img
+                      width="50"
+                      height="50"
+                      src={TwitterIcon}
+                      alt="Twitter"
+                    />
+                  ) : socialNetwork === "Spotify" ? (
+                    <img
+                      width="50"
+                      height="50"
+                      src={SpotifyIcon}
+                      alt="Spotify"
+                    />
+                  ) : socialNetwork === "Apple Music" ? (
+                    <img
+                      width="50"
+                      height="50"
+                      src={AppleMusicIcon}
+                      alt="Apple Music"
+                    />
+                  ) : socialNetwork === "Venmo" ? (
+                    <img width="50" height="50" src={VenmoIcon} alt="Venmo" />
+                  ) : socialNetwork === "CashApp" ? (
+                    <img
+                      width="50"
+                      height="50"
+                      src={CashappIcon}
+                      alt="CashApp"
+                    />
+                  ) : socialNetwork === "Phone Number" ? (
+                    <img width="50" height="50" src={PhoneIcon} alt="Phone" />
+                  ) : socialNetwork === "Email" ? (
+                    <img width="50" height="50" src={EmailIcon} alt="Email" />
+                  ) : socialNetwork === "Website" ? (
+                    <img
+                      width="50"
+                      height="50"
+                      src={WebsiteIcon}
+                      alt="Website"
+                    />
+                  ) : socialNetwork === "Address" ? (
+                    <img
+                      width="50"
+                      height="50"
+                      src={MapPinIcon}
+                      alt="Google Maps"
+                    />
+                  ) : socialNetwork === "SMS" ? (
+                    <img width="50" height="50" src={SmsIcon} alt="SMS" />
+                  ) : socialNetwork === "Paypal" ? (
+                    <img width="50" height="50" src={PaypalIcon} alt="Paypal" />
+                  ) : socialNetwork === "Telegram" ? (
+                    <img
+                      width="50"
+                      height="50"
+                      src={TelegramIcon}
+                      alt="Telegram"
+                    />
+                  ) : socialNetwork === "OnlyFans" ? (
+                    <img
+                      width="50"
+                      height="50"
+                      src={OnlyFansIcon}
+                      alt="OnlyFans"
+                    />
+                  ) : socialNetwork === "GoFundMe" ? (
+                    <img
+                      width="50"
+                      height="50"
+                      src={GoFundMeIcon}
+                      alt="GoFundMe"
+                    />
+                  ) : socialNetwork === "Twitch" ? (
+                    <img width="50" height="50" src={TwitchIcon} alt="Twitch" />
+                  ) : socialNetwork === "Discord" ? (
+                    <img
+                      width="50"
+                      height="50"
+                      src={DiscordIcon}
+                      alt="Discord"
+                    />
+                  ) : socialNetwork === "HouseParty" ? (
+                    <img
+                      width="50"
+                      height="50"
+                      src={HousePartyIcon}
+                      alt="HouseParty"
+                    />
+                  ) : null}
+                </div>
+                <div className="d-flex justify-content-center">
+                  <div className="d-none d-sm-block">{socialNetwork}</div>
+                </div>
+              </div>
+            </a>
+          </div>
+        </>
+      ) : null}
+    </>
+  );
+}
+
+export default Row;
