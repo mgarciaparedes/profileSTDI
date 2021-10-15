371a69507c39e20a3a8455e316ce7c0bf882a1d2
mgarciaparedes <46930317+mgarciaparedes@users.noreply.github.com>
2021-10-15 00:29:41 -0300

    feat: input tipo file con estilo bootstrap y validar que sea requerido con al menos un archivo adjuntado y los formatos de los archivos tienen que ser siempre imágenes png, jpg y jpeg

--- src/app/views/AdvancedTools/childrenComponents/GallerySetup.js
+++ src/app/views/AdvancedTools/childrenComponents/GallerySetup.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Modal, Form, InputGroup, Button, Alert } from "react-bootstrap";
 import * as Icon from "react-bootstrap-icons";
-// import { Formik } from "formik";
+import { Formik } from "formik";
 import Swal from "sweetalert2";
-// import * as Yup from "yup";
+import * as Yup from "yup";
 import axios from "axios";
 import { AppContext } from "../../../../components/AppContext";
 import { SpinnerLoading } from "../../../../components/SpinnerLoading";
@@ -13,13 +13,56 @@ function GallerySetup() {
   const { objLogin, setGalleryActiveContext } = useContext(AppContext);
   const [gallery, setGallery] = useState([]);
   const [saveGalleryButton, setSaveGalleryButton] = useState(false);
+  const [filesLength, setFilesLength] = useState(0);
   const [loading, setLoading] = useState(false);
   const [galleryActive, setGalleryActive] = useState(objLogin.galleryActive);
+  const [isValidFile, setIsValidFile] = useState(false);
 
   //Variables para modal con info (primero)
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  useEffect(() => {
+    for (var i = 0; i < gallery.length; i++) {
+      if (
+        gallery[i].type === "image/jpeg" ||
+        gallery[i].type === "image/jpg" ||
+        gallery[i].type === "image/png"
+      ) {
+        setIsValidFile(true);
+      } else {
+        setIsValidFile(false);
+      }
+    }
+  }, [gallery]);
+
+  const schema = Yup.object().shape({
+    attachedDocument: Yup.mixed().required("At least one file is required"),
+    // .test(
+    //   "fileSize",
+    //   "Tamaño del archivo debe ser máximo de 1 MB",
+    //   (value) => {
+    //     return fileSize <= 1000000;
+    //   }
+    // )
+    // .test(
+    //   "fileType",
+    //   "Formatos de archivo soportados PDF, JPG, JPEG, PNG, DOC, DOCX, DOCM, RTF",
+    //   (value) => {
+    //     return (
+    //       fileType === "application/pdf" ||
+    //       fileType === "image/jpg" ||
+    //       fileType === "image/jpeg" ||
+    //       fileType === "image/png" ||
+    //       fileType === "application/msword" || //DOC y RTF
+    //       fileType ===
+    //         "application/vnd.openxmlformats-officedocument.wordprocessingml.document" || //DOCX
+    //       fileType === "application/vnd.ms-word.document.macroEnabled.12"
+    //     ); //DOCM
+    //   }
+    // ),
+  });
 
   const activateGallery = (e) => {
     setGalleryActive(e.target.checked);
@@ -37,7 +80,7 @@ function GallerySetup() {
     axios
       .post("/users/activateGallery", payload)
       .then((res) => {
-        console.log(res.data);
+        //console.log(res.data);
         setLoading(false);
       })
       .catch((error) => {
@@ -47,57 +90,109 @@ function GallerySetup() {
 
   const saveGallery = () => {
     setSaveGalleryButton(true);
-    //aquí comparo si el usuario ya tiene una galería previamente registrada
-    //si gallery viene como null, quiere decir que no hay registros y se porcerá a usar el servicio saveNewGallery
-    //por el contrario, si tiene ya registros, solo se deberá modificar el registro que ya tiene guardado.
-    if (gallery !== null) {
-      alert("El usuario tiene galería");
+
+    if (!isValidFile) {
       setSaveGalleryButton(false);
+      Swal.fire({
+        title: "Error",
+        text: "Format files must be .jpg, .jpeg or .png",
+        icon: "error",
+        confirmButtonText: "Try again",
+      });
     } else {
-      let formData = new FormData();
-      formData.append("galleryActive", true);
-      for (var x = 0; x < gallery.length; x++) {
-        formData.append("galleryImages", gallery[x]);
-      }
+      //aquí comparo si el usuario ya tiene una galería previamente registrada
+      //si gallery viene como null, quiere decir que no hay registros y se porcerá a usar el servicio saveNewGallery
+      //por el contrario, si tiene ya registros, solo se deberá modificar el registro que ya tiene guardado.
+      if (gallery !== null) {
+        let formData2 = new FormData();
+        formData2.append("galleryActive", objLogin.galleryActive);
+        for (var x = 0; x < gallery.length; x++) {
+          formData2.append("galleryImages", gallery[x]);
+        }
 
-      axios
-        .post("/users/saveNewGallery", formData, {
-          headers: {
-            "content-type": "multipart/form-data",
-          },
-        })
-        .then((res) => {
-          setSaveGalleryButton(false);
+        axios
+          .post("/users/updateGallery", formData2, {
+            headers: {
+              "content-type": "multipart/form-data",
+            },
+          })
+          .then((res) => {
+            setSaveGalleryButton(false);
 
-          const { ok, msg } = res.data;
+            const { ok, msg } = res.data;
 
-          if (ok && msg === "Gallery created succesfully.") {
-            Swal.fire({
-              title: "Process succesfully",
-              text: msg,
-              icon: "success",
-              confirmButtonText: "Ok",
-            });
-            handleClose();
-            setGalleryActive(true);
-            setGalleryActiveContext(true);
-          } else {
+            if (ok && msg === "Gallery updated succesfully.") {
+              Swal.fire({
+                title: "Process succesfully",
+                text: msg,
+                icon: "success",
+                confirmButtonText: "Ok",
+              });
+              handleClose();
+            } else {
+              Swal.fire({
+                title: "Error",
+                text: msg,
+                icon: "error",
+                confirmButtonText: "Try again",
+              });
+            }
+          })
+          .catch((error) => {
+            setSaveGalleryButton(false);
             Swal.fire({
               title: "Error",
-              text: msg,
+              text: "We are sorry, an error occurred.",
               icon: "error",
               confirmButtonText: "Try again",
             });
-          }
-        })
-        .catch((error) => {
-          Swal.fire({
-            title: "Error",
-            text: "we are sorry, an error occurred.",
-            icon: "error",
-            confirmButtonText: "Try again",
           });
-        });
+      } else {
+        let formData = new FormData();
+        formData.append("galleryActive", true);
+        for (var x = 0; x < gallery.length; x++) {
+          formData.append("galleryImages", gallery[x]);
+        }
+
+        axios
+          .post("/users/saveNewGallery", formData, {
+            headers: {
+              "content-type": "multipart/form-data",
+            },
+          })
+          .then((res) => {
+            setSaveGalleryButton(false);
+
+            const { ok, msg } = res.data;
+
+            if (ok && msg === "Gallery created succesfully.") {
+              Swal.fire({
+                title: "Process succesfully",
+                text: msg,
+                icon: "success",
+                confirmButtonText: "Ok",
+              });
+              handleClose();
+              setGalleryActive(true);
+              setGalleryActiveContext(true);
+            } else {
+              Swal.fire({
+                title: "Error",
+                text: msg,
+                icon: "error",
+                confirmButtonText: "Try again",
+              });
+            }
+          })
+          .catch((error) => {
+            Swal.fire({
+              title: "Error",
+              text: "we are sorry, an error occurred.",
+              icon: "error",
+              confirmButtonText: "Try again",
+            });
+          });
+      }
     }
   };
 
@@ -133,21 +228,45 @@ function GallerySetup() {
         backdrop="static"
         keyboard={false}
       >
-        <Modal.Header>
-          <Modal.Title>
-            <Icon.Images className="mb-1" /> Set up your gallery
-          </Modal.Title>
-        </Modal.Header>
+        <Formik
+          validationSchema={schema}
+          onSubmit={saveGallery}
+          initialValues={{
+            attachedDocument: "",
+          }}
+        >
+          {({
+            handleSubmit,
+            handleChange,
+            setFieldValue,
+            values,
+            touched,
+            isValid,
+            errors,
+          }) => (
+            <Form
+              onSubmit={handleSubmit}
+              noValidate
+              autoComplete="off"
+              name="addServiceData"
+              id="addServiceData"
+            >
+              <Modal.Header>
+                <Modal.Title>
+                  <Icon.Images className="mb-1" /> Set up your gallery
+                </Modal.Title>
+              </Modal.Header>
 
-        <Modal.Body>
-          <div className="container">
-            <div className="row">
-              <div className="col-sm-12 col-md-12 col-lg-12 m-auto">
-                <Alert variant="info">
-                  <Icon.InfoCircleFill className="mb-1" /> &nbsp; You need to
-                  choose at once the pictures you might set into your gallery.
-                </Alert>
-                {/*<p>
+              <Modal.Body>
+                <div className="container">
+                  <div className="row">
+                    <div className="col-sm-12 col-md-12 col-lg-12 m-auto">
+                      <Alert variant="info">
+                        <Icon.InfoCircleFill className="mb-1" /> &nbsp; You need
+                        to choose at once the pictures you might set into your
+                        gallery.
+                      </Alert>
+                      {/*<p>
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                   do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                   Ut enim ad minim veniam, quis nostrud exercitation ullamco
@@ -157,52 +276,94 @@ function GallerySetup() {
                   cupidatat non proident, sunt in culpa qui officia deserunt
                   mollit anim id est laborum.
                 </p> */}
-                <Form.Group controlId="formFileMultiple">
-                  <Form.Label>Set up your gallery:</Form.Label>
-                  <Form.Control
-                    type="file"
-                    multiple
-                    onChange={(e) => {
-                      setGallery(e.target.files);
-                    }}
-                  />
-                </Form.Group>
-              </div>
-            </div>
-          </div>
-        </Modal.Body>
-        <Modal.Footer className="col-lg-12">
-          <Button
-            variant="light"
-            onClick={() => {
-              handleClose();
-            }}
-          >
-            Close
-          </Button>
-          <Button
-            type="button"
-            variant="primary"
-            disabled={saveGalleryButton === true}
-            onClick={() => {
-              saveGallery();
-            }}
-          >
-            <div className="d-flex d-inline-block justify-content-center">
-              <span
-                className="spinner-border spinner-border-sm mt-1 mr-2"
-                role="status"
-                style={{
-                  display: saveGalleryButton === true ? "inline-block" : "none",
-                }}
-                aria-hidden="true"
-              ></span>
-              {saveGalleryButton === true
-                ? " Loading, please wait..."
-                : "Save Gallery"}
-            </div>
-          </Button>
-        </Modal.Footer>
+
+                      <Form.Group controlId="formFileMultiple">
+                        {/* <Form.Label>Set up your gallery:</Form.Label>
+                        <Form.Control
+                          type="file"
+                          multiple
+                          onChange={(e) => {
+                            setGallery(e.target.files);
+                          }}
+                        /> */}
+
+                        <Form.File custom>
+                          <Form.File.Input
+                            multiple
+                            id="attachedDocument"
+                            name="attachedDocument"
+                            onChange={(e) => {
+                              handleChange(e);
+                              setFilesLength(e.target.files.length);
+                              setGallery(e.target.files);
+                              console.log(e.target.files);
+                            }}
+                            isValid={
+                              !!touched.attachedDocument &&
+                              !errors.attachedDocument
+                            }
+                            // isValid={
+                            //   !!touched.attachedDocument && isValidFile === true
+                            // }
+                            isInvalid={
+                              !!touched.attachedDocument &&
+                              !!errors.attachedDocument
+                            }
+                          />
+
+                          <Form.File.Label data-browse="Seleccionar">
+                            {filesLength > 0
+                              ? filesLength === 1
+                                ? filesLength + " file selected"
+                                : filesLength + " files selected"
+                              : "Select files..."}
+                          </Form.File.Label>
+
+                          <Form.Control.Feedback type="invalid">
+                            {errors.attachedDocument}
+                          </Form.Control.Feedback>
+                        </Form.File>
+                      </Form.Group>
+                    </div>
+                  </div>
+                </div>
+              </Modal.Body>
+              <Modal.Footer className="col-lg-12">
+                <Button
+                  variant="light"
+                  onClick={() => {
+                    handleClose();
+                  }}
+                >
+                  Close
+                </Button>
+                <Button
+                  type="submit"
+                  variant="primary"
+                  disabled={saveGalleryButton === true}
+                  // onClick={() => {
+                  //   saveGallery();
+                  // }}
+                >
+                  <div className="d-flex d-inline-block justify-content-center">
+                    <span
+                      className="spinner-border spinner-border-sm mt-1 mr-2"
+                      role="status"
+                      style={{
+                        display:
+                          saveGalleryButton === true ? "inline-block" : "none",
+                      }}
+                      aria-hidden="true"
+                    ></span>
+                    {saveGalleryButton === true
+                      ? " Loading, please wait..."
+                      : "Save Gallery"}
+                  </div>
+                </Button>
+              </Modal.Footer>
+            </Form>
+          )}
+        </Formik>
       </Modal>
     </div>
   );
